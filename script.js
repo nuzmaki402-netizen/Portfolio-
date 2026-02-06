118b003d23aae1130209ec2b17a844867838534e
nuzmaki402-netizen <nuzmaki402@gmail.com>
2026-02-06 10:22:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -112,9 +112,3 @@ btn.addEventListener("click", () => {
 
 
 
-const menu = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav-links');
-
-menu.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
